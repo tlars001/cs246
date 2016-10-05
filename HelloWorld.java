fe53eda4f80b06e85e1ce7c77837f79ce8d80cb4
Trevor Larson <trevor-90@hotmail.com>
2016-10-05 11:22:38 -0600

    Merged the Files

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("Hello GitHub!");
-=======
-		System.out.println("Hello World!");
-		System.out.println("Testing...");
->>>>>>> test
+		System.out .println("Testing...");
 	}
 }
